067a75b1d10229229912f556185b05ce7cc47583
mati.sarralde <mati.sarralde@gmail.com>
2025-09-17 02:36:21 +0000

    Set up CI/CD for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
       plugins: [react()],
+      base: '/paseos-de-blanca/',
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
